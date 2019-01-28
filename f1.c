3c069baf92178ff3ce70290d575b73671445e27b
IndumathiMeenakshi <indumathi.meenakshi@vvdntech.in>
2019-01-28 15:25:36 +0530

    Initial commit

---
+++ f1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int add(int a,int b);
+int sub(int a,int b);
+int mul(int a.int b);
+int div(int a,int b);
+int mod(int a,int b);
+int add(int a,int b)
+{
+return a+b;
+}
+int sub(int a,int b)
+{
+return a-b;
+}
+int mul(int a,int b)
+{
+return a*b;
+}
+int div(int a,int b)
+{
+return a/b;
+}
+int mod(int a,int b)
+{
+return a%b;
+}
+int main()
+{
+int n,m;
+scanf("%d%d",&n,&m);
+v=add(n,m);
+w=sub(n,m);
+x=mul(n,m);
+y=div(n,m);
+z=mod(n,m);
+printf("%d%d%d%d%d",v,w,x,y,z);
+return 0;
+}
